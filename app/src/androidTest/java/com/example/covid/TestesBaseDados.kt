ff9f135299ff6736f2d4f84833c0db0f0bdc26ef
Pedro Matos <Pedro Matos@DESKTOP-80M85SD>
2021-05-20 16:47:30 +0100

    Adição teste para apagar base de dados

--- app/src/androidTest/java/com/example/covid/TestesBaseDados.kt
+++ app/src/androidTest/java/com/example/covid/TestesBaseDados.kt
@@ -7,6 +7,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,11 +19,14 @@ class TestesBaseDados {
     private fun getAppContext() = InstrumentationRegistry.getInstrumentation().targetContext
     private fun getCovidOpenHelper() = BdCovidOpenHelper(getAppContext())
 
+    @Before
+    fun apagaBaseDados() {
+        getAppContext().deleteDatabase(BdCovidOpenHelper.NOME_BASE_DADOS)
+    }
     @Test
     fun consegueAbrirBaseDados() {
         val db = getCovidOpenHelper().readableDatabase
         assert(db.isOpen)
         db.close()
     }
-
 }
