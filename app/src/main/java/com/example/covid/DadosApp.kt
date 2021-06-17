ad24e180eda43d897d2f8930cd83ee1110df80f8
Pedro Matos <Pedro Matos@DESKTOP-80M85SD>
2021-06-17 17:23:42 +0100

    criada a classe DadosApp

---
+++ app/src/main/java/com/example/covid/DadosApp.kt
@@ -0,0 +1,10 @@
+package com.example.covid
+
+
+class DadosApp {
+        companion object {
+            lateinit var activity: MainActivity
+            lateinit var ListaPacientesFragment: ListaPacientesFragment
+            var pacienteSelecionado : Paciente? = null
+        }
+}
