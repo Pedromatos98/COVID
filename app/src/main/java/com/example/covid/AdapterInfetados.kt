e60f73a3052222ff58b60f5873f1c9dee6e66fc1
Pedro Matos <Pedro Matos@DESKTOP-80M85SD>
2021-07-03 22:25:00 +0100

    Adicionado código para preencher RecyclerViewInfetados

--- app/src/main/java/com/example/covid/AdapterInfetados.kt
+++ app/src/main/java/com/example/covid/AdapterInfetados.kt
@@ -1,4 +1,113 @@
 package com.example.covid
 
-class AdapterInfetados {
+import android.database.Cursor
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterInfetados (val fragment: ListaInfetadosFragment) : RecyclerView.Adapter<AdapterInfetados.ViewHolderInfetado>(){
+    public var cursor: Cursor? = null
+        get() = field
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    class ViewHolderInfetado(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        private val textViewDataInfecao = itemView.findViewById<TextView>(R.id.textViewDataInfecao)
+        private val textViewSintomas = itemView.findViewById<TextView>(R.id.textViewSintomas)
+
+
+        private lateinit var infetado: Infetado
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun atualizaInfetado(infetado: Infetado) {
+            this.infetado = infetado
+
+            textViewDataInfecao.text = infetado.dataInfecao
+            textViewSintomas.text = infetado.sintomas
+
+        }
+
+        override fun onClick(v: View?) {
+
+        }
+
+        /**
+         * Called when a view has been clicked.
+         *
+         * @param v The view that was clicked.
+         */
+
+
+
+    }
+
+    /**
+     * Called when RecyclerView needs a new [ViewHolder] of the given type to represent
+     * an item.
+     *
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     *
+     *
+     * The new ViewHolder will be used to display items of the adapter using
+     * [.onBindViewHolder]. Since it will be re-used to display
+     * different items in the data set, it is a good idea to cache references to sub views of
+     * the View to avoid unnecessary [View.findViewById] calls.
+     *
+     * @param parent The ViewGroup into which the new View will be added after it is bound to
+     * an adapter position.
+     * @param viewType The view type of the new View.
+     *
+     * @return A new ViewHolder that holds a View of the given view type.
+     * @see .getItemViewType
+     * @see .onBindViewHolder
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderInfetado {
+        val itemInfetado = fragment.layoutInflater.inflate(R.layout.item_infetado, parent, false)
+
+        return ViewHolderInfetado(itemInfetado)
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the [ViewHolder.itemView] to reflect the item at the given
+     * position.
+     *
+     *
+     * Note that unlike [android.widget.ListView], RecyclerView will not call this method
+     * again if the position of the item changes in the data set unless the item itself is
+     * invalidated or the new position cannot be determined. For this reason, you should only
+     * use the `position` parameter while acquiring the related data item inside
+     * this method and should not keep a copy of it. If you need the position of an item later
+     * on (e.g. in a click listener), use [ViewHolder.getAdapterPosition] which will
+     * have the updated adapter position.
+     *
+     * Override [.onBindViewHolder] instead if Adapter can
+     * handle efficient partial bind.
+     *
+     * @param holder The ViewHolder which should be updated to represent the contents of the
+     * item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    override fun onBindViewHolder(holder: ViewHolderInfetado, position: Int) {
+        cursor!!.moveToPosition(position)
+        holder.atualizaInfetado(Infetado.fromCursor(cursor!!))
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
+    override fun getItemCount(): Int {
+        return cursor?.count ?: 0
+    }
 }
--- app/src/main/java/com/example/covid/ListaInfetadosFragment.kt
+++ app/src/main/java/com/example/covid/ListaInfetadosFragment.kt
@@ -1,59 +1,81 @@
 package com.example.covid
 
+import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.covid.databinding.FragmentListaInfetadosBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  * Use the [ListaInfetadosFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ListaInfetadosFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class ListaInfetadosFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor>{
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentListaInfetadosBinding? = null
+    private var adapterInfetados : AdapterInfetados? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        DadosApp.fragment = this
+        (activity as MainActivity).menuAtual = R.menu.menu_lista_pacientes
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista_infetados, container, false)
+        _binding = FragmentListaInfetadosBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerViewLivros = view.findViewById<RecyclerView>(R.id.RecyclerViewInfetados)
+        adapterInfetados = AdapterInfetados(this)
+        recyclerViewLivros.adapter = adapterInfetados
+        recyclerViewLivros.layoutManager = LinearLayoutManager(requireContext())
+
+        LoaderManager.getInstance(this)
+            .initLoader(ID_LOADER_MANAGER_INFETADOS, null, this)
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        return CursorLoader(
+            requireContext(),
+            ContentProviderCovid.ENDERECO_INFETADOS,
+            TabelaInfetados.TODAS_COLUNAS,
+            null,null,
+            TabelaInfetados.CAMPO_DATA_INFECAO
+        )
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        adapterInfetados!!.cursor = data
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        adapterInfetados!!.cursor = null
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListaInfetadosFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListaInfetadosFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val ID_LOADER_MANAGER_INFETADOS = 0
     }
 }
