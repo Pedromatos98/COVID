354fc7b702eab40f39bf0644b06054fa390fa1b8
Pedro Matos <Pedro Matos@DESKTOP-80M85SD>
2021-07-03 19:02:32 +0100

    criado o fragmento ListaInfetados

--- app/src/main/java/com/example/covid/MenuPrincipalFragment.kt
+++ app/src/main/java/com/example/covid/MenuPrincipalFragment.kt
@@ -3,7 +3,6 @@ package com.example.covid
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -17,10 +16,6 @@ import androidx.navigation.fragment.findNavController
 class MenuPrincipalFragment : Fragment() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,10 +26,12 @@ class MenuPrincipalFragment : Fragment() {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_menu_principal, container, false)
 
-        val botaoPacientes = v.findViewById<Button>(R.id.button)
-        botaoPacientes.setOnClickListener{
+        val botaoPacientes = v.findViewById<Button>(R.id.buttonPacientes)
+        botaoPacientes.setOnClickListener {
             findNavController().navigate(R.id.action_menuPrincipalFragment_to_ListaPacientesFragment)
+
         }
+
         return v
     }
 
