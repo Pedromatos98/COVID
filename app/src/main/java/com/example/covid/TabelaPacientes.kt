63e191c52c0600f7410e3c1856891c492a848be7
Pedro Matos <Pedro Matos@DESKTOP-80M85SD>
2021-07-07 18:23:22 +0100

    Adicionado campo Morada

--- app/src/main/java/com/example/covid/TabelaPacientes.kt
+++ app/src/main/java/com/example/covid/TabelaPacientes.kt
@@ -8,7 +8,7 @@ import android.provider.BaseColumns
 class TabelaPacientes (db: SQLiteDatabase) {
     private val db: SQLiteDatabase = db
     fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME_PACIENTE TEXT NOT NULL, $CAMPO_NUMERO_UTENTE INTEGER NOT NULL, $CAMPO_DATA_NASCIMENTO INTEGER NOT NULl,$CAMPO_CONTACTO TEXT NOT NULL )")
+        db.execSQL("CREATE TABLE $NOME_TABELA (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME_PACIENTE TEXT NOT NULL, $CAMPO_NUMERO_UTENTE INTEGER NOT NULL, $CAMPO_DATA_NASCIMENTO INTEGER NOT NULl,$CAMPO_MORADA TEXT NOT NULL,$CAMPO_CONTACTO TEXT NOT NULL )")
     }
     fun insert(values: ContentValues): Long {
         return db.insert(NOME_TABELA, null, values)
@@ -38,9 +38,10 @@ class TabelaPacientes (db: SQLiteDatabase) {
         const val CAMPO_NOME_PACIENTE = "nome_paciente"
         const val CAMPO_NUMERO_UTENTE = "numero_utente"
         const val CAMPO_DATA_NASCIMENTO = "data_nascimento"
+        const val CAMPO_MORADA = "morada"
         const val CAMPO_CONTACTO = "contacto"
 
         val TODAS_COLUNAS = arrayOf(BaseColumns._ID, CAMPO_NOME_PACIENTE, CAMPO_NUMERO_UTENTE,
-            CAMPO_DATA_NASCIMENTO, CAMPO_CONTACTO)
+            CAMPO_DATA_NASCIMENTO, CAMPO_MORADA,CAMPO_CONTACTO)
     }
 }
